61c681c95535d7c91155526ba465f24323e4e351
Sergeyakzhik <sergeyakzhik@gmail.com>
2018-11-30 17:48:21 +0300

    fix bugs and add validation in redux-form, get rid of AddEvent component

--- src/containers/AddEventFieldContainer.js
+++ src/containers/AddEventFieldContainer.js
@@ -1,54 +1,39 @@
 import React from 'react';
-import '../components/AddEventField/AddEventField.css';
-import AddEventForm from '../components/AddEventField/AddEventForm/AddEventForm';
+import AddEventForm from './AddEventForm/AddEventForm';
+import { reduxForm, formValueSelector } from 'redux-form';
 
 import { connect } from 'react-redux';
-import {
-  closeAddEventField,
-  changeStartDate,
-  changeEndDate,
-  changeEventName,
-  changeEventDescription,
-  changeEventPlace
-} from '../store/actions/addEventFieldActions';
+import { closeAddEventField } from '../store/actions/addEventFieldActions';
 import { addEvent } from '../store/actions/calendarActions';
 
 import "react-datepicker/dist/react-datepicker.css";
 
 class AddEventFieldContainer extends React.Component {
 
-  handleStartDateChange = date => {
-    const { endDate } = this.props;
-
-    this.props.changeStartDate(date);
-    this.props.changeEndDate(date > endDate ? date : endDate);
-  }
-
-  handleEndDateChange = date => {
-    this.props.changeEndDate(date);
-  }
-
-  getCurrentTime = () => {
-    let date = new Date();
-
-    return [date.getHours(), date.getMinutes()];
-  }
-
   submit = e => {
     this.handleEndButtonClick();
-    this.props.addEvent(Object.assign({}, this.props.event));
+    this.props.addEvent(this.fillEventData());
   }
+
+  fillEventData = () => {
+    const {
+      startDate,
+      endDate,
+      name,
+      place,
+      description
+    } = this.props;
+
+    return {
+      startDate,
+      endDate,
+      name,
+      place,
+      description
+    }
+  };
 
   handleEndButtonClick = e => this.props.closeAddEventField(false);
-
-  handleInputChange = e => {
-    if(e.target.id === 'title')
-      this.props.changeEventName(e.target.value);
-    if(e.target.id === 'description')
-      this.props.changeEventDescription(e.target.value);
-    if(e.target.id === 'place')
-      this.props.changeEventPlace(e.target.value);
-  }
 
   render() {
     const { startDate, endDate } = this.props;
@@ -61,8 +46,6 @@ class AddEventFieldContainer extends React.Component {
         </div>
         <AddEventForm
           onSubmit={this.submit}
-          handleStartDateChange={this.handleStartDateChange}
-          handleEndDateChange={this.handleEndDateChange}
         />
       </div>
     );
@@ -72,22 +55,27 @@ class AddEventFieldContainer extends React.Component {
 const mapStateToProps = store => ({
   isActive: store.eventField.isActive,
   event: store.eventField.event,
-  startDate: store.eventField.event.startDate,
-  endDate: store.eventField.event.endDate,
   length: store.eventField.event.length,
 });
 
 const mapDispatchToProps = dispatch => ({
   closeAddEventField: isActive => dispatch(closeAddEventField(isActive)),
-  changeStartDate: startDate => dispatch(changeStartDate(startDate)),
-  changeEndDate: endDate => dispatch(changeEndDate(endDate)),
   addEvent: event => dispatch(addEvent(event)),
-  changeEventName: name => dispatch(changeEventName(name)),
-  changeEventDescription: description => dispatch(changeEventDescription(description)),
-  changeEventPlace: place => dispatch(changeEventPlace(place))
 });
 
+const selector = formValueSelector('addEvent');
+
+AddEventFieldContainer = connect(
+  state => selector(
+    state,
+    'startDate',
+    'endDate',
+    'name',
+    'place',
+    'description'
+  )
+)(AddEventFieldContainer)
+
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+  mapStateToProps
 )(AddEventFieldContainer);
---
+++ src/containers/AddEventForm/AddEventForm.js
@@ -0,0 +1,135 @@
+import React from 'react';
+import moment from "moment";
+import { Field, Fields, reduxForm, formValueSelector } from 'redux-form';
+import DatePicker from "react-datepicker";
+import './AddEventForm.css';
+
+import { connect } from 'react-redux';
+import {
+  changeStartDate,
+  changeEndDate
+} from '../../store/actions/addEventFieldActions';
+
+const initialData = {
+  startDate: moment(),
+  endDate: moment()
+}
+
+const required = value => value ? undefined : 'Required';
+
+const eventNameInput = ({ input, type, id, placeholder, meta: { error, touched } }) => (
+  <div>
+    <input {...input} placeholder={placeholder} type={type} id={id} className="form-control"/>
+  </div>
+)
+
+class AddEventForm extends React.Component {
+
+  renderDateFields = fields => {                              //FIX: get rid of dates props of event object. Help: https://github.com/erikras/redux-form/issues/369
+    const { startDate, endDate } = fields;
+    const startDateValue = startDate.input.value;
+    const endDateValue = endDate.input.value;
+    const event = this.props.event;
+
+    let handleStartDateChange = date => {
+      startDate.input.onChange(date);
+      this.props.changeStartDate(date);
+
+      if(date > endDateValue) {
+        endDate.input.onChange(date);
+        this.props.changeEndDate(date);
+      }
+    }
+
+    let handleEndDateChange = date => {
+      endDate.input.onChange(date);
+      this.props.changeEndDate(date);
+
+      if(date < startDateValue) {
+        startDate.input.onChange(date);
+        this.props.changeStartDate(date);
+      }
+    }
+
+    return (
+      <>
+        <div className="form-group col-sm-6 text-center">
+          <label htmlFor="dateTimeStart" className="col-form-label">From</label>
+          <DatePicker
+            selected={event.startDate}
+            onChange={handleStartDateChange}
+            minDate={moment()}
+            showTimeSelect
+            timeIntervals={15}
+            dateFormat="LLL"
+            timeCaption="Time"
+          />
+        </div>
+        <div className="form-group col-sm-6 text-center">
+          <label htmlFor="dateTimeEnd" className="col-form-label">To</label>
+          <DatePicker
+            selected={event.endDate}
+            onChange={handleEndDateChange}
+            minDate={startDateValue}
+            showTimeSelect
+            timeIntervals={15}
+            dateFormat="LLL"
+            timeCaption="Time"
+          />
+        </div>
+      </>
+    )
+  };
+
+  render() {
+    const { onSubmit, submitting, valid } = this.props;
+
+    return (
+      <form onSubmit={onSubmit}>
+        <div className="form-row">
+          <div className="form-group col-sm-6 text-center">
+            <label htmlFor="name">Title</label>
+            <Field name="name" component={eventNameInput} type="text" validate={required} id="name" placeholder="Title*" />
+          </div>
+          <div className="form-group col-sm-6 text-center">
+            <label htmlFor="place">Place</label>
+            <Field name="place" component="input" type="text" className="form-control" id="place" placeholder="Place" />
+          </div>
+          <Fields names={[ 'startDate', 'endDate' ]} component={this.renderDateFields} />
+          <div className="form-group col-sm-12 text-center">
+            <label htmlFor="comment">Description</label>
+            <Field name="description" component="textarea" type="text" rows="5" className="form-control" id="description" placeholder="Description" />
+          </div>
+          <div className="col-sm-12 text-center">
+            <button type="submit" disabled={!valid || submitting} className="btn btn-primary add-event-button">Submit</button>
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
+
+const mapStateToProps = store => ({
+  event: store.eventField.event
+});
+
+const mapDispatchToProps = dispatch => ({
+  changeStartDate: startDate => dispatch(changeStartDate(startDate)),
+  changeEndDate: endDate => dispatch(changeEndDate(endDate))
+});
+
+AddEventForm = reduxForm({
+  form: 'addEvent'
+})(AddEventForm);
+
+const selector = formValueSelector('addEvent');
+
+AddEventForm = connect(
+  state => ({ initialValues: initialData }),
+  state => selector(state, 'startDate', 'endDate')
+)(AddEventForm);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AddEventForm);
